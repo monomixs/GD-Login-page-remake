1bae385f1d4312b47d963510163d86346123e06b
monomixs <wedleygouverneur14@gmail.com>
2026-02-07 21:48:19 -0500

    Fix redirect path in login functionality
    Updated the redirect path for home.html in the login logic.

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ loginBtn.addEventListener('click', () => {
         localStorage.setItem('gdUsername', username);
         
         // Redirect to home.html
-        window.location.href = "home.html";
+        window.location.href = "../home.html";
     } else {
         alert("Please enter a username!");
     }
@@ -27,4 +27,5 @@ if(openModal) {
     closeModal.onclick = () => modal.style.display = 'none';
 
 }
+
 
